8da58a9d3a6ed526f02a857d8990f5e1af1a34b1
Steven McCord <steve@icxmedia.com>
2018-02-09 15:14:48 -0500

    Point to the icxmedia clojars namespace

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject org.clojars.jgerman/onyx-elasticsearch "0.9.15.1-SNAPSHOT"
+(defproject com.icxmedia/onyx-elasticsearch "0.9.15.1-SNAPSHOT"
   :description "Onyx plugin for Elasticsearch"
   :url "https://github.com/onyx-platform/onyx-elasticsearch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[s3-wagon-private "1.2.0"]]
   :repositories {"snapshots" {:url "https://clojars.org/repo"
                               :username :env
                               :password :env
@@ -10,7 +11,10 @@
                  "releases" {:url "https://clojars.org/repo"
                              :username :env
                              :password :env
-                             :sign-releases false}}
+                             :sign-releases false}
+                 "icxmedia" {:url "s3p://maven.icxmedia.com/releases/"
+                             :creds :gpg
+                             :update :always}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.9.15"]
